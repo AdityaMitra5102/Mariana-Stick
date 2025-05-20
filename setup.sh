acb46ddd963793680b82bfc6b238a981088e3f19
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-20 18:13:40 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt -y upgrade
+echo "deb [trusted=yes] https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu/ jammy main" | sudo tee /etc/apt/sources.list.d/deadsnakes-ubuntu-ppa-lunar.list
+sudo apt update
+sudo apt install -y python3.12
+sudo rm -rf $(which python3)
+sudo ln -sT $(which python3.12) /usr/bin/python3
+curl https://bootstrap.pypa.io/get-pip.py | python3
+curl https://AdityaMitra5102.github.io/Project-Mariana/setup.sh | sudo sh
+sudo service mariana stop
+python3 -m pip install -r /etc/mar/requirements.txt --break-system-package --upgrade
+sudo python3 -m pip install -r /etc/mar/requirements.txt --break-system-package --upgrade
+sudo service mariana restart
+
+sudo cp -f config.txt /etc/firmware/config.txt
+sudo cp -f cmdline.txt /etc/firmware/cmdline.txt
+sudo cp -f modules /etc/modules
+sudo cp -f usb-gadget.sh /usr/local/sbin/usb-gadget.sh
+sudo chmod +x /usr/local/sbin/usb-gadget.sh
+sudo cp -f usbgadget.service /lib/systemd/system/usbgadget.service
+sudo systemctl daemon-reload
+sudo systemctl enable usbgadget.service
+
+sudo nmcli con add type bridge ifname br0
+sudo nmcli con add type bridge-slave ifname usb0 master br0
+sudo nmcli con add type bridge-slave ifname usb1 master br0
+sudo nmcli connection modify bridge-br0 ipv4.method manual ipv4.addresses 10.55.0.1/24
+sudo apt install -y dnsmasq
+sudo cp -f br0 /etc/dnsmasq.d/br0
+
+sudo apt install -y apache2
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_balancer
+sudo a2enmod lbmethod_byrequests
+sudo cp -f 000-default.conf /etc/apache2/sites-enabled/000-default.conf
---
+++ wifihandler.py
@@ -0,0 +1,35 @@
+from flask import *
+from wifiscan import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/networks')
+def networks():
+	return jsonify(scan_wifi())
+	
+@app.route('/connect', methods=['POST'])
+def connect():
+	ssid=request.form.get('ssid')
+	password=request.form.get('password')
+	if connect_wifi(ssid, password):
+		return 'Success'
+	else:
+		return 'Fail', 400
+		
+@app.route('/wifiname')
+def wifiname():
+	ssid=get_connected_ssid()
+	if ssid is not None:
+		return ssid
+	return 'Fail', 400
+	
+@app.route('/marconn')
+def marconn():
+	return 'true'
+	
+if __name__=='__main__':
+	app.run('0.0.0.0', 5000)
