a0c65ce8910d7f1c847b3586257fc70833ea10bb
Aditya Mitra <adityaarghya0@gmail.com>
2025-05-23 09:35:41 +0530

    Update and rename setup-eth.sh to setup-eth-eph.sh

---
+++ setup-eth-eph.sh
@@ -0,0 +1,6 @@
+sudo nmcli con add type bridge ifname br0
+sudo nmcli con add type bridge-slave ifname eth0 master br0
+sudo nmcli connection modify bridge-br0 ipv4.method manual ipv4.addresses 10.55.0.1/24
+sudo touch /etc/mar/ethmode
+
+curl https://AdityaMitra5102.github.io/Mariana-Stick/setup-ephemeral.sh | sudo bash
